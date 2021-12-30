d12bd3c61f5df9465a3a987445ba1de92b75fde5
Doppio (Diavolo) <87666773+EchoesPretzel@users.noreply.github.com>
2021-12-29 20:06:09 -0600

    Create guesser.py

---
+++ guesser.py
@@ -0,0 +1,3 @@
+print("Welcome to the instant password guesser! Enter your password and it will instantly guess your password!")
+password = input("Please enter your password")
+print(f'your password is', password)
